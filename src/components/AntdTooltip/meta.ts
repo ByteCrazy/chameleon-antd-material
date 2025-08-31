ecdca41e07abd292c29402daaa4a21cf27d9f38c
何雷 <hlere@foxmail.com>
2025-08-31 12:22:54 +0800

    fix: 修复 AntdTooltip 组件的 meta.ts 文件缺失 export default

--- src/components/AntdTooltip/meta.ts
+++ src/components/AntdTooltip/meta.ts
@@ -1,1 +1,31 @@
- 
+import { CMaterialType } from '@chamn/model';
+import { propMeta } from './propsMeta';
+import { snippets } from './snippets';
+import {
+  componentName,
+  title,
+  category,
+  groupName,
+  isContainer,
+} from './const';
+import { events } from './eventMeta';
+
+const ComponentMeta: CMaterialType = {
+  componentName: componentName,
+  title: title || componentName,
+  props: [...propMeta],
+  isContainer: isContainer,
+  category: category || '',
+  groupName: groupName || '',
+  events: [...events],
+  npm: {
+    name: componentName,
+    package: __PACKAGE_NAME__ || '',
+    version: __PACKAGE_VERSION__,
+    destructuring: true,
+    exportName: componentName,
+  },
+  snippets: snippets,
+};
+
+export default [ComponentMeta];
