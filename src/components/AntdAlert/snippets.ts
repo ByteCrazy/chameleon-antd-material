785e63add2b381fcf3efb1bdfa1268b8131e46cb
何雷 <hlere@foxmail.com>
2025-08-31 11:53:21 +0800

    fix: 修复 AntdAlert 组件的 export default 和空文件问题

--- src/components/AntdAlert/snippets.ts
+++ src/components/AntdAlert/snippets.ts
@@ -1,1 +1,16 @@
- 
+import { SnippetsType } from '@chamn/model';
+
+export const snippets: SnippetsType[] = [
+  {
+    title: 'Antd Alert',
+    snapshotText: 'AAL',
+    description: 'antd 警告提示',
+    schema: {
+      props: {
+        content: '这是一条警告提示',
+        type: 'warning',
+        showIcon: true,
+      },
+    },
+  },
+];
